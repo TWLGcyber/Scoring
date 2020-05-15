3477294e4346534ce5eb844111fbc8663c4771d2
TWLGcyber <34041366+TWLGcyber@users.noreply.github.com>
2020-05-15 13:50:44 -0400

    Update Engine.sh

--- Engine.sh
+++ Engine.sh
@@ -5,19 +5,19 @@
 
 
 if sudo cat /home/cyberpatriot/Desktop/Forensic1.txt |
-  grep -xqFe "Welcome to Greenbrier Middle!"
+  grep -F "Welcome to Greenbrier Middle!"
 then
   echo "You have answered Forensic Question 1 correctly." >> /home/cyberpatriot/Desktop/ScoringReport.txt
   aplay /etc/Cyber/gain.wav
 fi
 
-if grep -xqFe "DBiggers" /home/cyberpatriot/Desktop/Forensic2.txt; then
+if grep -F "DBiggers" /home/cyberpatriot/Desktop/Forensic2.txt; then
   echo "You have answered Forensic Question 2 correctly." >> /home/cyberpatriot/Desktop/ScoringReport.txt
   aplay /etc/Cyber/gain.wav
 fi
 
 if sudo cat /home/cyberpatriot/Desktop/Forensic3.txt |
-  grep -xqFe "Rainbow, you suck at siege!"
+  grep -F "Rainbow, you suck at siege!"
 then
   echo "You have answered Forensic Question 3 correctly." >> /home/cyberpatriot/Desktop/ScoringReport.txt
   aplay /etc/Cyber/gain.wav
@@ -25,7 +25,7 @@ fi
 
 
 if sudo cat /etc/lightdm/lightdm.conf |
-  grep -xqFe "allow-guest=false"
+  grep -F "allow-guest=false"
 then
   echo "You have correctly disabled the guest user." >> /home/cyberpatriot/Desktop/ScoringReport.txt
   aplay /etc/Cyber/gain.wav
