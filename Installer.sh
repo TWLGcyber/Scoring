27c0d97cd1a8424689204d6b2ae1195842e3f12c
TWLGcyber <34041366+TWLGcyber@users.noreply.github.com>
2020-05-15 02:27:31 -0400

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Would you like to install the Scoring Engine? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    sudo mv Engine.sh /home/cyberpatriot/Desktop
+    sudo mv Forensic1.txt /home/cyberpatriot/Desktop
+    sudo mv Forensic2.txt /home/cyberpatriot/Desktop
+    sudo mv Forensic3.txt /home/cyberpatriot/Desktop
+    sudo mv ScoringReport.txt /home/cyberpatriot/Desktop
+    sudo chmod 777 Engine.sh
+    sudo crontab -l | { cat; echo "* * * * * sudo bash /home/cyberpatriot/Desktop/Engine.sh"; } | crontab -
+fi
