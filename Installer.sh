6c3140b98e7fb7b1d9ad5809849b378b87eaeabf
TWLGcyber <34041366+TWLGcyber@users.noreply.github.com>
2020-05-15 22:57:16 -0400

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -3,8 +3,8 @@
 read -p "Are all the vulnerabilities set-up?" -n 1 -r
 if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
-    sudo chmod 777 /home/$USER/Scoring/Vulns/Configuration.sh
-    sudo bash /home/$USER/Scoring/Vulns/Configuration.sh
+    sudo chmod 777 /home/cyberpatriot/Scoring/Vulns/Configuration.sh
+    sudo bash /home/cyberpatriot/Scoring/Vulns/Configuration.sh
 fi
 
 read -p "Would you like to install the Scoring Engine?" -n 1 -r
@@ -12,14 +12,14 @@ if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
     sudo mkdir /etc/Cyber
     sudo mv Engine.sh /etc/Cyber
-    sudo mv Forensic1.txt /home/$USER/Desktop
-    sudo mv Forensic2.txt /home/$USER/Desktop
-    sudo mv Forensic3.txt /home/$USER/Desktop
-    sudo mv ScoringReport.txt /home/$USER/Desktop
+    sudo mv Forensic1.txt /home/cyberpatriot/Desktop
+    sudo mv Forensic2.txt /home/cyberpatriot/Desktop
+    sudo mv Forensic3.txt /home/cyberpatriot/Desktop
+    sudo mv ScoringReport.txt /home/cyberpatriot/Desktop
     sudo mv gain.wav /etc/Cyber
     sudo chmod 777 /etc/Cyber/Engine.sh
     #This next part deletes all of the files
-    sudo rm -rf /home/$USER/Scoring
+    sudo rm -rf /home/cyberpatriot/Scoring
     #Reboots the machine
     sudo reboot
 fi
