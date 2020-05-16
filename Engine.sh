e74911f84ef589b668d9dccfeda74b2e3e713365
TWLGcyber <34041366+TWLGcyber@users.noreply.github.com>
2020-05-15 22:44:04 -0400

    Update Engine.sh

--- Engine.sh
+++ Engine.sh
@@ -4,16 +4,22 @@
 #Scoring report is located on the desktop as ScoringReport.txt
 
 
-if sudo cat /home/cyberpatriot/Desktop/Forensic1.txt |
-  grep -F "Welcome to Greenbrier Middle!"
-then
+if grep -F "Welcome to Greenbrier Middle!" /home/cyberpatriot/Desktop/Forensic1.txt; then
+  if grep -F "You have answered Forensic Question 1 correctly." /home/cyberpatriot/Desktop/ScoringReport.txt; then
+    ls
+  else
   echo "You have answered Forensic Question 1 correctly." >> /home/cyberpatriot/Desktop/ScoringReport.txt
   aplay /etc/Cyber/gain.wav
+  fi
 fi
 
 if grep -F "DBiggers" /home/cyberpatriot/Desktop/Forensic2.txt; then
+  if grep -F "You have answered Forensic Question 2 correctly." /home/cyberpatriot/Desktop/ScoringReport.txt; then
+    ls
+  else
   echo "You have answered Forensic Question 2 correctly." >> /home/cyberpatriot/Desktop/ScoringReport.txt
   aplay /etc/Cyber/gain.wav
+  fi
 fi
 
 if sudo cat /home/cyberpatriot/Desktop/Forensic3.txt |
@@ -23,10 +29,20 @@ then
   aplay /etc/Cyber/gain.wav
 fi
 
-
-if sudo cat /etc/lightdm/lightdm.conf |
-  grep -F "allow-guest=false"
-then
-  echo "You have correctly disabled the guest user." >> /home/cyberpatriot/Desktop/ScoringReport.txt
+if grep -F "Rainbow, you suck at siege!" /home/cyberpatriot/Desktop/Forensic3.txt; then
+  if grep -F "You have answered Forensic Question 3 correctly." /home/cyberpatriot/Desktop/ScoringReport.txt; then
+    ls
+  else
+  echo "You have answered Forensic Question 3 correctly." >> /home/cyberpatriot/Desktop/ScoringReport.txt
   aplay /etc/Cyber/gain.wav
+  fi
+fi
+
+if grep -F "allow-guest=false" /etc/lightdm/lightdm.conf.d/*.conf; then
+  if grep -F "You have answered correctly disabled the guest user." /home/cyberpatriot/Desktop/ScoringReport.txt; then
+    ls
+  else
+  echo "You have answered correctly disabled the guest user." >> /home/cyberpatriot/Desktop/ScoringReport.txt
+  aplay /etc/Cyber/gain.wav
+  fi
 fi
