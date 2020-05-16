5dffc50e5ff2059f89c5d94e21dce6e7c38514f8
TWLGcyber <34041366+TWLGcyber@users.noreply.github.com>
2020-05-15 22:46:42 -0400

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -4,14 +4,14 @@ read -p "Would you like to install the Scoring Engine? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
-    sudo mv Engine.sh /home/cyberpatriot/Desktop
+    sudo mkdir /etc/Cyber
+    sudo mv Engine.sh /etc/Cyber
     sudo mv Forensic1.txt /home/cyberpatriot/Desktop
     sudo mv Forensic2.txt /home/cyberpatriot/Desktop
     sudo mv Forensic3.txt /home/cyberpatriot/Desktop
     sudo mv ScoringReport.txt /home/cyberpatriot/Desktop
-    sudo mkdir /etc/Cyber
     sudo mv gain.wav /etc/Cyber
-    sudo chmod 777 /home/cyberpatriot/Desktop/Engine.sh
+    sudo chmod 777 /etc/Cyber/Engine.sh
     #This next part deletes all of the files
     sudo rm -rf /home/cyberpatriot/Scoring
 fi
